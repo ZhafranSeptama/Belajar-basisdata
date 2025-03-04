83b640796cf7f5d459c3bc3aa96a0ec8ac43035e
japran <zhafranseptama04@gmail.com>
2025-03-04 18:05:59 +0700

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -1,37 +1,62 @@
 sql DDL di CLI
+  
 -- copynya satu satu masukin ke cmd
   
--- create database 
+-- CREATE DATABASE 
 
   create database mahasiswa;
 
--- use database 
+-- USE DATABASE (Khusus CLI)
 
   use mahasiswa;
 
--- bikin table 
+-- CREATE TABLE 
 
   create table data_siswa (nim int(11),nama char(50),alamat char(30));
 
--- bikin row 
+-- Make Row  
 
   INSERT INTO `data_siswa`(`nim`, `nama`, `alamat`) VALUES (1234,'japran','bekasi')
 
--- alter table add column 
+-- ALTER TABLE (ADD COLUMN) 
 
   ALTER TABLE mahasiswa ADD jurusan varchar(50);
 
--- alter table multi column
+-- ALTER TABLE (ADD MULTI COLUMN)
 
   alter table data_siswa add (fakultas varchar(50), jurusan2 varchar(50));
 
--- alter table modify column 
+-- ALTER TABLE (MODIFY COLUMN) 
 
   alter table data_siswa modify nama varchar(60);
 
--- create index 
+-- CREATE INDEX 
 
   CREATE UNIQUE INDEX unik ON data_siswa (alamat);
+
+-- ALTER DROP (DROP COLUMN)
+
+  alter table data_siswa drop column jurusan2;
+
+-- ALTER RENAME (CHANGE FIELD)
+
+  alter table data_siswa change fakultas fakultas1 varchar(50);
+
+-- DROP DATABASE
+
+  DROP DATABASE Mahasiswa; (hati-hati nyobanya)
+
+-- DROP TABLE
+
+  DROP TABLE MAHASISWA; (Hati-hati nyobanya)
+
+-- DROP INDEX
+
+  DROP INDEX MHSIDX;
+
+-- DROP VIEW
+
+  DROP VIEW MHSVIEW;
 
 -- #not null with default 
 
